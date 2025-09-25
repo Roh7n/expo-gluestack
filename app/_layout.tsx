316b09e4d051a73fe9b18621d3a0e2fb13a1c7d5
Rohan Baburaj <rohanbaburaj4446@gmail.com>
2025-09-25 18:36:27 +0530

    fixed dropdown list

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,23 +4,28 @@ import { StatusBar } from "expo-status-bar";
 import { useColorScheme } from "react-native";
 import "../global.css";
 
-import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
-import '@/global.css';
-import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import "@/global.css";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { useEffect } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
-    "Nunito-Black": require("../assets/fonts/Nunito-Black.ttf"),
-    "Nunito-Bold": require("../assets/fonts/Nunito-Bold.ttf"),
-    "Nunito-ExtraBold": require("../assets/fonts/Nunito-ExtraBold.ttf"),
     "Nunito-Light": require("../assets/fonts/Nunito-Light.ttf"),
     "Nunito-Medium": require("../assets/fonts/Nunito-Medium.ttf"),
     "Nunito-Regular": require("../assets/fonts/Nunito-Regular.ttf"),
     "Nunito-SemiBold": require("../assets/fonts/Nunito-SemiBold.ttf"),
+    "Nunito-Bold": require("../assets/fonts/Nunito-Bold.ttf"),
+    "Nunito-ExtraBold": require("../assets/fonts/Nunito-ExtraBold.ttf"),
+    "Nunito-Black": require("../assets/fonts/Nunito-Black.ttf"),
   });
 
- const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme();
 
   useEffect(() => {
     if (fontsLoaded) {
@@ -33,12 +38,13 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-    <GluestackUIProvider mode="system">
-      <StatusBar style="auto"/>
-      <Stack screenOptions={{ headerShown: false }} />
-    </GluestackUIProvider>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <SafeAreaProvider>
+        <GluestackUIProvider mode="system">
+          <StatusBar style="auto" />
+          <Stack screenOptions={{ headerShown: false }} />
+        </GluestackUIProvider>
+      </SafeAreaProvider>
     </ThemeProvider>
-  
   );
 }
